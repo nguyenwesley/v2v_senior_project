624546b5a8511145d7c7b2eff8188d783a3404da
nguyenw <wnguyen370@gmail.com>
2020-12-13 15:12:16 -0800

    Added pseudocode from Spring 2020 for Final Documentation purposes

---
+++ Spring 2020 Pseudocode/dataHandling.h
@@ -0,0 +1,9 @@
+#ifndef _DATAHANDLING_H_
+#define _DATAHANDLING_H_
+
+//Modify prototypes to include function variables
+void sendData();
+void receiveData();
+void parseData();
+void compressData();
+#endif
---
+++ Spring 2020 Pseudocode/tool.c
@@ -0,0 +1,37 @@
+//Our own created .h files
+#include "tool.h"
+
+
+/*
+*	External libraries that will be used.When a ned library is add,
+*	append a comment to the library explaining why it is needed.
+*/
+#include <stdio.h>					//Standard C library
+
+
+void bZeroArray()
+{
+	//Initialize array values
+}
+
+
+void bZero()
+{
+	//Initialize variable/structure values
+}
+
+
+//May not be required
+void enQ()
+{
+	//Enqueue a value to the tail of an array
+}
+
+
+//May not be required
+int deQ()
+{
+	//Dequeue the head value to a variable to return
+}
+
+
